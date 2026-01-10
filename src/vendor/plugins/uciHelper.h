dcd2ad17a73b6ba66895d0b63e15a5c927600e20
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-09 23:44:26 -0300

    fixed uci dependency

--- src/vendor/plugins/uciHelper.h
+++ src/vendor/plugins/uciHelper.h
@@ -1,6 +1,6 @@
 #ifndef UCI_HELPER
 #define UCI_HELPER
 
-int GetStringValue(char *path, char *fallback, char *buf, int len)
+int GetStringValue(char *path, char *fallback, char *buf, int len);
 
 #endif
