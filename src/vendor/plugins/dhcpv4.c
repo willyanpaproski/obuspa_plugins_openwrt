c1c0f64c63a1f23ba5cfa209048df616aeb14380
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-10 00:40:11 -0300

    fix: fixed types

--- src/vendor/plugins/dhcpv4.c
+++ src/vendor/plugins/dhcpv4.c
@@ -8,16 +8,28 @@
 int GetLeaseTime(dm_req_t *req, char *buf, int len)
 {
     (void)req;
-
-    int err = GetStringValue("dhcp.lan.leasetime", "0", buf, len);
+    char raw_value[64];
+    
+    int err = GetStringValue("dhcp.lan.leasetime", "12h", raw_value, sizeof(raw_value));
     if (err != USP_ERR_OK) {
         return err;
     }
 
-    size_t str_len = strlen(buf);
-    if (str_len > 0 && buf[str_len - 1] == 's') {
-        buf[str_len - 1] = '\0';
+    char *endptr;
+    long value = strtol(raw_value, &endptr, 10);
+    long seconds = value;
+
+    if (*endptr != '\0') {
+        switch (*endptr) {
+            case 'm': seconds = value * 60; break;
+            case 'h': seconds = value * 3600; break;
+            case 'd': seconds = value * 86400; break;
+            case 's': seconds = value; break;
+            default:  seconds = value; break; 
+        }
     }
+
+    snprintf(buf, len, "%ld", seconds);
 
     return USP_ERR_OK;
 }
