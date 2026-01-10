f68e13ce77723b99e4a9c39736d5bbf5ec402982
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-10 00:50:22 -0300

    fix: fixed uci helper

--- src/vendor/plugins/dhcpv4.c
+++ src/vendor/plugins/dhcpv4.c
@@ -8,28 +8,6 @@
 int GetLeaseTime(dm_req_t *req, char *buf, int len)
 {
     (void)req;
-    char raw_value[64];
     
-    int err = GetStringValue("dhcp.lan.leasetime", "12h", raw_value, sizeof(raw_value));
-    if (err != USP_ERR_OK) {
-        return err;
-    }
-
-    char *endptr;
-    long value = strtol(raw_value, &endptr, 10);
-    long seconds = value;
-
-    if (*endptr != '\0') {
-        switch (*endptr) {
-            case 'm': seconds = value * 60; break;
-            case 'h': seconds = value * 3600; break;
-            case 'd': seconds = value * 86400; break;
-            case 's': seconds = value; break;
-            default:  seconds = value; break; 
-        }
-    }
-
-    snprintf(buf, len, "%ld", seconds);
-
-    return USP_ERR_OK;
+    return GetStringValue("dhcp.lan.leasetime", "12h", buf, len);
 }
--- src/vendor/plugins/uciHelper.c
+++ src/vendor/plugins/uciHelper.c
@@ -1,35 +1,42 @@
 #include "uciHelper.h"
 #include "usp_err_codes.h"
+
 #include <uci.h>
 #include <string.h>
 #include <stdio.h>
 
 int GetStringValue(char *path, char *fallback, char *buf, int len)
 {
-    struct uci_context *ctx = NULL;
+    struct uci_context *ctx;
     struct uci_ptr ptr;
-    int res = USP_ERR_OK;
+    char uci_path[64];
 
-    if (path == NULL || buf == NULL || len <= 0) return USP_ERR_INTERNAL_ERROR;
+    if (path == NULL || buf == NULL || len <= 0) {
+        return USP_ERR_INTERNAL_ERROR;
+    }
+
+    snprintf(uci_path, sizeof(uci_path), "%s", path);
 
     ctx = uci_alloc_context();
-    if (ctx == NULL) return USP_ERR_INTERNAL_ERROR;
+    if (ctx == NULL) {
+        return USP_ERR_INTERNAL_ERROR;
+    }
 
-    char path_copy[128];
-    snprintf(path_copy, sizeof(path_copy), "%s", path);
+    memset(&ptr, 0, sizeof(ptr));
 
-    if (uci_lookup_ptr(ctx, &ptr, path_copy, true) != UCI_OK || 
-        ptr.o == NULL || 
+    if (uci_lookup_ptr(ctx, &ptr, uci_path, true) != UCI_OK ||
+        ptr.o == NULL ||
         ptr.o->type != UCI_TYPE_STRING ||
-        ptr.o->v.string == NULL) 
+        ptr.o->v.string == NULL ||
+        ptr.o->v.string[0] == '\0')
     {
-        snprintf(buf, len, "%s", fallback);
-    } 
-    else 
+        snprintf(buf, len, fallback);
+    }
+    else
     {
         snprintf(buf, len, "%s", ptr.o->v.string);
     }
 
     uci_free_context(ctx);
-    return res;
+    return USP_ERR_OK;
 }
