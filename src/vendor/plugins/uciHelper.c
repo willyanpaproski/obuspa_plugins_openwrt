fd264b8b33b5adfd9dc02171c1e64905d75c9599
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-11 13:04:05 -0300

    teste

--- src/vendor/plugins/uciHelper.c
+++ src/vendor/plugins/uciHelper.c
@@ -9,10 +9,13 @@ int GetStringValue(char *path, char *buf, int len)
 {
     struct uci_context *ctx;
     struct uci_ptr ptr;
+    char uci_path[64];
 
     if (path == NULL || buf == NULL || len <= 0) {
         return USP_ERR_INTERNAL_ERROR;
     }
+
+    snprintf(uci_path, sizeof(uci_path), "%s", path);
 
     ctx = uci_alloc_context();
     if (ctx == NULL) {
@@ -21,10 +24,11 @@ int GetStringValue(char *path, char *buf, int len)
 
     memset(&ptr, 0, sizeof(ptr));
 
-    if (uci_lookup_ptr(ctx, &ptr, path, true) != UCI_OK ||
+    if (uci_lookup_ptr(ctx, &ptr, uci_path, true) != UCI_OK ||
         ptr.o == NULL ||
         ptr.o->type != UCI_TYPE_STRING ||
-        ptr.o->v.string == NULL)
+        ptr.o->v.string == NULL ||
+        ptr.o->v.string[0] == '\0')
     {
         buf[0] = '\0';
     }
@@ -32,54 +36,6 @@ int GetStringValue(char *path, char *buf, int len)
     {
         snprintf(buf, len, "%s", ptr.o->v.string);
     }
-
-    uci_free_context(ctx);
-    return USP_ERR_OK;
-}
-
-int GetListValues(const char *path,
-                  char values[][64],
-                  int max_values,
-                  int value_len,
-                  int *out_count)
-{
-    struct uci_context *ctx;
-    struct uci_ptr ptr;
-    struct uci_element *e;
-    int count = 0;
-
-    if (!path || !values || !out_count || max_values <= 0 || value_len <= 0) {
-        return USP_ERR_INTERNAL_ERROR;
-    }
-
-    *out_count = 0;
-
-    ctx = uci_alloc_context();
-    if (!ctx) {
-        return USP_ERR_INTERNAL_ERROR;
-    }
-
-    memset(&ptr, 0, sizeof(ptr));
-
-    if (uci_lookup_ptr(ctx, &ptr, (char *)path, true) != UCI_OK ||
-        ptr.o == NULL ||
-        ptr.o->type != UCI_TYPE_LIST)
-    {
-        uci_free_context(ctx);
-        return USP_ERR_OK;
-    }
-
-    uci_foreach_element(&ptr.o->v.list, e)
-    {
-        if (count >= max_values) {
-            break;
-        }
-
-        snprintf(values[count], value_len, "%s", e->name);
-        count++;
-    }
-
-    *out_count = count;
 
     uci_free_context(ctx);
     return USP_ERR_OK;
