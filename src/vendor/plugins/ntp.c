9455bdd6513c6095851de6842d08cac4cba479ed
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-11 13:08:13 -0300

    testess

--- src/vendor/plugins/ntp.c
+++ src/vendor/plugins/ntp.c
@@ -1,32 +1,32 @@
-#include "ntp.h"
-#include "usp_err_codes.h"
-#include "uciHelper.h"
+// #include "ntp.h"
+// #include "usp_err_codes.h"
+// #include "uciHelper.h"
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int GetNtpServerAddress(dm_req_t *req, char *buf, int len)
-{
-    char servers[8][64];
-    int count = 0;
-    int index;
+// int GetNtpServerAddress(dm_req_t *req, char *buf, int len)
+// {
+//     char servers[8][64];
+//     int count = 0;
+//     int index;
 
-    if (req == NULL ||
-        req->inst == NULL ||
-        req->inst->order < 1)
-    {
-        buf[0] = '\0';
-        return USP_ERR_OK;
-    }
+//     if (req == NULL ||
+//         req->inst == NULL ||
+//         req->inst->order < 1)
+//     {
+//         buf[0] = '\0';
+//         return USP_ERR_OK;
+//     }
 
-    GetListValues("system.ntp.server", servers, 8, 64, &count);
+//     GetListValues("system.ntp.server", servers, 8, 64, &count);
 
-    index = req->inst->instances[0] - 1;
+//     index = req->inst->instances[0] - 1;
 
-    if (index < 0 || index >= count) {
-        buf[0] = '\0';
-        return USP_ERR_OK;
-    }
+//     if (index < 0 || index >= count) {
+//         buf[0] = '\0';
+//         return USP_ERR_OK;
+//     }
 
-    snprintf(buf, len, "%s", servers[index]);
-    return USP_ERR_OK;
-}
+//     snprintf(buf, len, "%s", servers[index]);
+//     return USP_ERR_OK;
+// }
