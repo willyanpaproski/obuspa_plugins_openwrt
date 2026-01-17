3dd357cb00743dbc9889a12a2081f8b4e32efcaf
willyan.bueno <willyan.bueno@ixcsoft.com.br>
2026-01-17 16:49:05 -0300

    dhcp tests

--- src/vendor/plugins/dhcpv4.c
+++ src/vendor/plugins/dhcpv4.c
@@ -46,36 +46,104 @@ int SetLeaseTime(dm_req_t *req, char *buf)
 
 int GetGateway(dm_req_t *req, char *buf, int len)
 {
-    (void)req;
+    char dhcpOptions[16][64] = {0};
+    int count = 0;
+    bool found = false;
 
-    return GetStringValue("network.lan.gateway", buf, len);
+    if (GetListValues("dhcp.lan.dhcp_option", dhcpOptions, 16, 64, &count) != USP_ERR_OK) {
+        return USP_ERR_INTERNAL_ERROR;
+    }
+
+    for (int i = 0; i < count; i++) {
+        if (strncmp(dhcpOptions[i], "3,", 2) == 0) {
+            snprintf(buf, len, "%s", dhcpOptions[i] + 2);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) buf[0] = '\0';
+
+    return USP_ERR_OK;
 }
 
 int SetGateway(dm_req_t *req, char *buf)
 {
-    if (buf == NULL) return USP_ERR_INTERNAL_ERROR;
+    char dhcpOptions[16][64] = {0};
+    int count = 0;
+    bool found = false;
 
-    int ret = SetStringValue("network.lan.gateway", buf);
+    if (GetListValues("dhcp.lan.dhcp_option", dhcpOptions, 16, 64, &count) != USP_ERR_OK) {
+        return USP_ERR_INTERNAL_ERROR;
+    }
 
-    if (ret == USP_ERR_OK) system("/etc/init.d/dnsmasq restart");
+    for (int i = 0; i < count; i++) {
+        if (strncmp(dhcpOptions[i], "3,", 2) == 0) {
+            snprintf(dhcpOptions[i], 64, "3,%s", buf);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) return USP_ERR_INTERNAL_ERROR;
+
+    int ret = SetListValues("dhcp.lan.dhcp_option", dhcpOptions, count);
+
+    if (ret != USP_ERR_OK) return USP_ERR_INTERNAL_ERROR;
+
+    system("/etc/init.d/dnsmasq restart");
 
     return ret;
 }
 
 int GetSubnetMask(dm_req_t *req, char *buf, int len)
 {
-    (void)req;
+    char dhcpOptions[16][64] = {0};
+    int count = 0;
+    bool found = false;
 
-    return GetStringValue("network.lan.netmask", buf, len);
+    if (GetListValues("dhcp.lan.dhcp_option", dhcpOptions, 16, 64, &count) != USP_ERR_OK) {
+        return USP_ERR_INTERNAL_ERROR;
+    }
+
+    for (int i = 0; i < count; i++) {
+        if (strncmp(dhcpOptions[i], "1,", 2) == 0) {
+            snprintf(buf, len, "%s", dhcpOptions[i] + 2);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) buf[0] = '\0';
+
+    return USP_ERR_OK;
 }
 
 int SetSubnetMask(dm_req_t *req, char *buf)
 {
-    if (buf == NULL) return USP_ERR_INTERNAL_ERROR;
+    char dhcpOptions[16][64] = {0};
+    int count = 0;
+    bool found = false;
 
-    int ret = SetStringValue("network.lan.netmask", buf);
+    if (GetListValues("dhcp.lan.dhcp_option", dhcpOptions, 16, 64, &count) != USP_ERR_OK) {
+        return USP_ERR_INTERNAL_ERROR;
+    }
 
-    if (ret == USP_ERR_OK) system("/etc/init.d/dnsmasq restart");
+    for (int i = 0; i < count; i++) {
+        if (strncmp(dhcpOptions[i], "1,", 2) == 0) {
+            snprintf(dhcpOptions[i], 64, "1,%s", buf);
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) return USP_ERR_INTERNAL_ERROR;
+
+    int ret = SetListValues("dhcp.lan.dhcp_option", dhcpOptions, count);
+
+    if (ret != USP_ERR_OK) return USP_ERR_INTERNAL_ERROR;
+
+    system("/etc/init.d/dnsmasq restart");
 
     return ret;
 }
